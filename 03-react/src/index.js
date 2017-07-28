629770432e79ba322bd94eb8d7aa6dc52b03f1cf
Nontawat Numor <nontawat39@gmail.com>
2017-07-28 11:53:30 +0700

    Add new component to App :tea:

--- 03-react/src/index.js
+++ 03-react/src/index.js
@@ -1,8 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
+const Title = () => (
+  <h1>Hello, React</h1>
+)
+
 const App = () => (
-  <h1>Hello, Ract!</h1>
+  <div>
+    <h2>App</h2>
+    <Title />
+  </div>
 )
 
 ReactDOM.render(

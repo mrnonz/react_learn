99e035fb4075420a81e9603ca0279d69700a07db
Nontawat Numor <nontawat39@gmail.com>
2017-07-28 13:35:57 +0700

    Console to view props :tea:

--- 04-react-app/src/App.js
+++ 04-react-app/src/App.js
@@ -2,8 +2,13 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-const App = ({text, name}) => (
-  <h1>{text}, {name}</h1>
-)
+// const App = ({text, name}) => (
+//   <h1>{text}, {name}</h1>
+// )
+
+const App = (props) => {
+  console.log(props)
+  return <h1>{props.text}, {props.name}</h1>
+}
 
 export default App;

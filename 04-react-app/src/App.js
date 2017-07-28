8dbaa94d2aa126045d89154a9f7f4ed24e871cdb
Nontawat Numor <nontawat39@gmail.com>
2017-07-28 13:47:20 +0700

    Remove comment :tea:

--- 04-react-app/src/App.js
+++ 04-react-app/src/App.js
@@ -2,15 +2,6 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-// const App = ({text, name}) => (
-//   <h1>{text}, {name}</h1>
-// )
-
-// const App = (props) => {
-//   console.log(props)
-//   return <h1>{props.text}, {props.name}</h1>
-// }
-
 class App extends React.Component {
   constructor(props) {
     super(props)
@@ -20,9 +11,6 @@ class App extends React.Component {
   }
 
   render() {
-    // return (
-    //   <h1>{this.props.text}, {this.props.name}</h1>
-    // )
     const {text, name} = this.props
     return (
       <h1>{text}, {name} -- {this.state.text}</h1>

c4856a92d6cdb1ad43520734c5383f5a3ab8b7e8
Nontawat Numor <nontawat39@gmail.com>
2017-07-28 13:31:34 +0700

    Add 2 props and send to child component :tea:

--- 04-react-app/src/App.js
+++ 04-react-app/src/App.js
@@ -3,7 +3,7 @@ import logo from './logo.svg';
 import './App.css';
 
 const App = (props) => (
-  <h1>{props.text}</h1>
+  <h1>{props.text}, {props.name}</h1>
 )
 
 export default App;
--- 04-react-app/src/index.js
+++ 04-react-app/src/index.js
@@ -5,7 +5,7 @@ import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(
-  <App text='Hi World'/>,
+  <App text='Hi World' name='MrNonz'/>,
   document.getElementById('root')
 )
 registerServiceWorker()

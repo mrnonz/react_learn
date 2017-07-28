0915bca9606ffac853d56060a62b6df4efe60d07
Nontawat Numor <nontawat39@gmail.com>
2017-07-28 14:33:58 +0700

    Add todo list :tea:

--- 05-todo/src/components/App.js
+++ 05-todo/src/components/App.js
@@ -1,9 +1,32 @@
 import React from 'react'
 
+const NewItem = () => (
+  <div>
+    <input type="text" />
+    <button>Add</button>
+  </div>
+)
+
+const TodoItem = ({ text }) => (
+  <li>{text}</li>
+)
+
+const TodoList = () => (
+  <ul>
+    <TodoItem text="Clean room"/>
+    <TodoItem text="Wash dishes"/>
+    <TodoItem text="Eat burger"/>
+  </ul>
+)
+
 class App extends React.Component {
   render() {
     return (
-      <h1>Todo</h1>
+      <div>
+        <h1>Todo</h1>
+        <NewItem />
+        <TodoList />
+      </div>
     )
   }
 }

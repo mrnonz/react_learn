e0a21d9ad2d77499cd105a7a4cac8e613094fe0e
Nontawat Numor <nontawat39@gmail.com>
2017-07-28 13:45:18 +0700

    Use state to render :tea:

--- 04-react-app/src/App.js
+++ 04-react-app/src/App.js
@@ -12,13 +12,20 @@ import './App.css';
 // }
 
 class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      text: 'Hello from the other site. (state)'
+    }
+  }
+
   render() {
     // return (
     //   <h1>{this.props.text}, {this.props.name}</h1>
     // )
     const {text, name} = this.props
     return (
-      <h1>{text}, {name}</h1>
+      <h1>{text}, {name} -- {this.state.text}</h1>
     )
   }
 }

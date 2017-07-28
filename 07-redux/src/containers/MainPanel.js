d74d44a285ead2e498402dcf74f377872c41b686
Nontawat Numor <nontawat39@gmail.com>
2017-07-28 17:32:41 +0700

    Add comtainer for send connect back to view :tea:

---
+++ 07-redux/src/containers/MainPanel.js
@@ -0,0 +1,12 @@
+import MainPanel from '../components/MainPanel'
+import { addItem } from '../actions/item'
+
+const mapStateToProps = (state) => ({
+  todo: state,
+})
+
+const mapDispatchToProps = (dispatch) => ({
+  addItem: (item) => dispatch(addItem(item)),
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(MainPanel)

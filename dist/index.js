70f5d958792eddb4b75b6fc8e2465e59bed3d15d
Nontawat Numor <nontawat39@gmail.com>
2017-07-28 10:38:25 +0700

    Try to build index :tea:

--- dist/index.js
+++ dist/index.js
@@ -10,3 +10,19 @@ console.log("Hello, React!");
 
 console.log((0, _math.add)(2, 4));
 console.log((0, _math.decrease)(99));
+
+var numbers = [1, 2, 3, 4, 5];
+var x2 = numbers.map(function (number) {
+  return number * 2;
+});
+console.log(x2);
+
+var even = numbers.filter(function (number) {
+  return number % 2 == 0;
+});
+console.log(even);
+
+var sum = numbers.reduce(function (total, number) {
+  return total + number;
+});
+console.log(sum);

424393ab2b0f58d6b1b0940c577bb5d12c850075
Nontawat Numor <nontawat39@gmail.com>
2017-07-28 13:38:37 +0700

    Create new class for App :tea:

--- 04-react-app/src/App.js
+++ 04-react-app/src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
@@ -6,9 +6,17 @@ import './App.css';
 //   <h1>{text}, {name}</h1>
 // )
 
-const App = (props) => {
-  console.log(props)
-  return <h1>{props.text}, {props.name}</h1>
+// const App = (props) => {
+//   console.log(props)
+//   return <h1>{props.text}, {props.name}</h1>
+// }
+
+class App extends React.Component {
+  render() {
+    return (
+      <h1>Hello</h1>
+    )
+  }
 }
 
 export default App;

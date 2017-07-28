8c0ea31e98bb5e97694de108945b526e5451e5ae
Nontawat Numor <nontawat39@gmail.com>
2017-07-28 10:32:26 +0700

    Create function for number :tea:

--- src/index.js
+++ src/index.js
@@ -4,3 +4,13 @@ import area, {add, decrease} from './math'
 
 console.log(add(2, 4))
 console.log(decrease(99))
+
+const numbers = [1, 2, 3, 4, 5]
+const x2 = numbers.map(
+  function (number, i) {
+    console.log('Index is ' + i)
+    return number * 2
+  }
+)
+
+console.log(x2)

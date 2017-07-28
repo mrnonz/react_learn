143b33399ab692b4c217c202b5682395c98a379f
Nontawat Numor <nontawat39@gmail.com>
2017-07-28 17:14:10 +0700

    Add item to use action :tea:

---
+++ 07-redux/src/reducers/item.js
@@ -0,0 +1,19 @@
+import { ADD_ITEM } from '../actions/types'
+
+const initialState = []
+
+const itemReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case ADD_ITEM:
+      const newItem = action.payload
+      return [
+        ...state,
+        newItem
+      ]
+    default: {
+      return state
+    }
+  }
+}
+
+export default itemReducer
